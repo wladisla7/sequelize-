b23eac9b40fac0a12cd604273d66357abf276ee6
f0rest <wladislawfedoseev@gmail.com>
2021-11-23 00:59:04 +0200

    recreate main index for server

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
-const express = require("express");
-const router = require("./routes");
 
-const app = express();
+const http = require("http");
+const app = require("./app");
+
 const PORT = 5000;
 
-app.get("/", (req, res) => {
-    res.send("Hello");
-});
-
-app.use("/api", router)
-
-app.listen(PORT, () => {
-    console.log(`App is lisening on PORT ${PORT}`);
+http.createServer(app).listen(PORT, () => {
+    console.log(`App is listening on PORT ${PORT}`);
 });
