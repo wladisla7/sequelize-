b0d154ab8164ce792c104609d519580b654f3d98
f0rest <wladislawfedoseev@gmail.com>
2021-11-23 00:56:44 +0200

    Create User Model

--- models/users.js
+++ models/users.js
@@ -1,38 +1,58 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
 
     static associate(models) {
+      User.hasMany(models.Order, { foreignKey: "user_id" });
     }
   };
   User.init({
     firstName: {
-      field:"firstName",
+      field: "firstName",
       type: DataTypes.STRING,
-      allowNull:false
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+        notNull: true
+      }
     },
     lastName: {
-      field:"lastName",
+      field: "lastName",
       type: DataTypes.STRING,
-      allowNull:false
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+        notNull: true
+      }
     },
     email: {
       type: DataTypes.STRING,
-      allowNull:false,
-      unique:true
+      unique: true,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+        notNull: true
+      }
     },
     birthday: {
-      type: DataTypes.DATA,
-      allowNull:false
+      type: DataTypes.DATE,
+      allowNull: false,
+      validate: {
+        isAfter(value) {
+          if (value => new Date()) {
+            throw new Error('18+')
+          }
+        },
+        notEmpty: true,
+        isDate: true
+      }
     },
   }, {
     sequelize,
     modelName: 'User',
-    tableName: 'users', 
-    underscore:true
+    tableName: 'users',
+    underscore: true
   });
   return User;
 };
