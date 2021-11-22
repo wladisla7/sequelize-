06f1b1c919df86fef46a72f47845dbeea47fc1d6
f0rest <wladislawfedoseev@gmail.com>
2021-11-23 00:57:18 +0200

    Changes for use routes

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const orderRouter = require("./orders.route")
 const userRouter = require("./users.route")
 
-router.get("/", (req, res) => {
-    res.send("test");
-})
- 
-router.use('/users', userRouter)
+router.use('/users', userRouter);
+router.use("/orders", orderRouter);
 
 module.exports = router;
