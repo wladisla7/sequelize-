92811d75d5485af8a305c409dc925831ec6d1520
f0rest <wladislawfedoseev@gmail.com>
2021-11-23 00:53:06 +0200

    Add CRUD for Users

--- routes/users.route.js
+++ routes/users.route.js
@@ -1,9 +1,18 @@
 const userRouter = require('express').Router();
+const userController = require("../controlles/users.controller");
+const { validateOrders } = require("../middlewares/users.mw");
+const { createOrder, getUserOrders } = require("../controlles/orders.controller");
 
-userRouter.get("/", (req, res) => {
-    console.log("users");
-    res.send();
-});
 
- 
+
+userRouter.get("/", userController.getAllUsers);
+userRouter.post("/", userController.createUser);
+
+userRouter.get("/:id", userController.getUserById);
+userRouter.patch("/:id", userController.updateUser);
+userRouter.delete("/:id", userController.deleteUser);
+
+userRouter.post("/:userId/orders", validateOrders, createOrder);
+userRouter.get("/:userId/orders", getUserOrders);
+
 module.exports = userRouter;
