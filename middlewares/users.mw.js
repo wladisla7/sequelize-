d5cce6701a9f575bfc76ea81060b9fdb657d21ae
f0rest <wladislawfedoseev@gmail.com>
2021-11-23 17:40:58 +0200

    solve name problem

--- middlewares/users.mw.js
+++ middlewares/users.mw.js
@@ -1,4 +1,4 @@
-module.exports.validateOrder = (req, res, next) => {
+module.exports.validateOrders = (req, res, next) => {
     const data = req.body;
     if (data.body === "") {
         return next(new TypeError("Body should not be empty string"));
